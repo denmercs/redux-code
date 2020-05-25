d2c85e9f5e4db86697313e8b712a29f43ab408a1
denmercs <denmercs@gmail.com>
2020-05-24 23:28:23 -0500

    comment on streamReducer

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,16 +1,17 @@
-import _ from 'lodash';
+import _ from "lodash";
 import {
   FETCH_STREAM,
   FETCH_STREAMS,
   CREATE_STREAM,
   EDIT_STREAM,
-  DELETE_STREAM
-} from '../actions/types';
+  DELETE_STREAM,
+} from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(action.payload, 'id') };
+      // did this because instead of an array the initial state is object: 12: {id: 12}
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
